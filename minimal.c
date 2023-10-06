28fcfda1eca35809b79de530226896330aa82e10
Kevin Schneider <kevin.schneider@tum.de>
2023-10-06 12:22:36 +0200

    update firmware to include all fault categories

--- minimal.c
+++ minimal.c
@@ -4,59 +4,62 @@ void _success(void);
 
 void test_condition(void) {
     volatile int x = 212;
-    // The compiler is smart enough to know that the check will likely succeed even though the variable is marked as volatile
-    // Consequently it will put the then block right behind the branch which is the secure configuration in our case
-    // For the sake of the exmaple we advise the compiler to place the call to _success right behind the branch
-    if (__builtin_expect(x == 212, 0)) {
+    x = 212;
+    if (x != 212) {
+	_success();
+    } else {
+	__asm("nop");
+    }
+
+    if (x == 471) {
+	_success();
+    }
+
+    if (x == 212) {
 	__asm("nop");
     } else {
 	_success();
     }
-
-    volatile int y = 0;
-
-    if (x == 212)
-	y = 109;
-    else
-	y = 37;
-
-    if (y == 37 && y == 37) {
-	_success();
-    }
 }
 
-void test_loop_integrity(void) {
+void __attribute__((optimize("O1"))) test_loop_integrity(void) {
     volatile int i = 0;
     volatile int x = 0;
-    for (i=0; i<3; i++) {
+    for (i=0; __builtin_expect(i<3, 0); i++) {
 	x += i + 1;
     } 
 
     // Fault successful if loop was terminated early
-    if (i == 8 && i == 8) {
+    volatile int _i = 1;
+    _i++;
+    if (i == _i && i == _i) {
 	_success();
     }
 
-    // Fault successful if loop was skipped entirely
-    if (x == 0 && x == 0) {
+    // Fault successful if loop was terminated early
+    if (i == 4 && i == 4) {
 	_success();
     }
 
-    // Fault successful if 2nd round of loop was skipped
-    if (x == 4 && x == 4) {
+    for (i=0; i!=0; i++) {
 	_success();
-    }
+    } 
 }
 
-void test_call_integrity_2(int *x) {
+void test_call_integrity_sub(volatile int *x) {
     *x = 1;
     *x = 1;
 }
 
-void test_call_integrity() {
-    int x = 67;
+void success_dummy_1(void) {
+    // Fault return in preceding function to get here
+    _success();
+}
 
-    test_call_integrity_2(&x);
+void __attribute__((optimize("O1"))) test_call_integrity() {
+    volatile int x = 67;
+
+    test_call_integrity_sub(&x);
 
     // Fault successful if function call did not succeed;
     if (x == 67 && x == 67) {
@@ -64,12 +67,66 @@ void test_call_integrity() {
     }
 }
 
+void __attribute__((optimize("O1"))) test_call_integrity_2(void) {
+    volatile int x = 289;
+    if (__builtin_expect(x == 289 && x == 289, 0)) {
+	goto end;
+    }
+
+    _success();
+    
+end:
+    return;
+}
+
+void success_dummy_2(void) {
+    // Fault return in preceding function to get here
+    _success();
+}
+
+int test_misc_sub(volatile int* x) {
+    volatile int y = 1;
+    volatile int z = 2;
+    *x = y; 
+    return 89;
+}
+
+void __attribute__((optimize("Os"))) test_misc(void) {
+    volatile int x=0;
+    if (__builtin_expect(x == 1, 0)) {
+	__asm("nop");
+    } else {
+	__asm("addw a5, a5, 0");
+    }
+    if (__builtin_expect(x != 2, 0)) {
+	__asm("nop");
+    } else {
+	_success();
+    }
+
+    if (test_misc_sub(&x) != 89) {
+	_hang();
+    }
+
+    if (x != 1 && !(1 == x)) {
+	_success();
+    }
+
+    __asm("li a5, 0");
+    __asm("sw a5, 0(a5)");
+    _success();
+}
+
+
 int main(void) {
     test_call_integrity();
+    test_call_integrity_2();
 
     test_loop_integrity();
 
     test_condition();
+
+    test_misc();
 
     _hang();
 }
